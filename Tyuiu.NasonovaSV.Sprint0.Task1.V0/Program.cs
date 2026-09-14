d0a8d0ce30ae05b11dd86bc7042d6e6d9c6c0c33
svetlananasonova <ytdyy6pg9h@privaterelay.appleid.com>
2026-09-14 17:21:53 +0500

    Task1

--- Tyuiu.NasonovaSV.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.NasonovaSV.Sprint0.Task1.V0/Program.cs
@@ -10,6 +10,11 @@ namespace Tyuiu.NasonovaSV.Sprint0.Task1.V0
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello,World");
+            string name = "Света";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет!");
+            Console.ReadKey();
         }
     }
 }
